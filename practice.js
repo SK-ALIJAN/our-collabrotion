24f7a80f39be31baefbff09a4520d70c58a895b5
Sk Alijan <106768235+SK-ALIJAN@users.noreply.github.com>
2022-11-30 13:14:19 +0530

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,4 +1,4 @@
-// To check the number if it is prime or not , if prime then say yes otherwise say no.
+// To check the number if it is prime or not , if prime then it will output yes otherwise  no.
 let num=13;
 let c=0;
 
@@ -8,3 +8,13 @@ while(i<num){
   i++;
 }
 (c==2)?(console.log("Yes")):(console.log("No"));
+
+
+// To check, it is palindrom or not . if pallindrome then it will output yes otherwise  no.
+
+let word="madam";
+let str="";
+for(let i=word.length;i<0;i++){
+str+=word[i]
+}
+(word==str)?(console.log("Yes")):(console.log("No"));
