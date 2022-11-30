ce6b7507380853b35347e1006beb27df06cbecc2
Sk Alijan <106768235+SK-ALIJAN@users.noreply.github.com>
2022-11-30 12:58:25 +0530

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,5 +1,10 @@
-let my_details={
-name:"sk alijan",
-city:"kolkata",
+// To check the number if it is prime or not , if prime then say yes otherwise say no.
+let num=13;
+let c=0;
+
+let i=0;
+while(i<num){
+  if(num%i!==0) c++
+  i++;
 }
-// this is my few details
+(c==2)?(console.log("Yes")):(console.log("No"));
